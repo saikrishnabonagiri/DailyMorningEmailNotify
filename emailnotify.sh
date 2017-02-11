1164d0f52cdcc9d457fcefed65a2c02b52ce7eaf
Saikrishna Bonagiri <saikrishna.bonagiri@gmail.com>
2017-02-11 18:18:46 -0500

    code modified 2

--- emailnotify.sh
+++ emailnotify.sh
@@ -1,1 +1,1 @@
-echo "hello world" | mail -s "Good Morning Saikrishna" saikrishna.bonagiri@gmail.com
+echo "Good Monring Saikrishna !! Have a Nice Day !!!" | mail -s "Good Morning Saikrishna" saikrishna.bonagiri@gmail.com
